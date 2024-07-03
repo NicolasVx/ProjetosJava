844b32ad4ddd4c07185cf439bfde6ded928cc23c
Nicolas <146778076+NicolasVx@users.noreply.github.com>
2024-07-03 11:22:43 -0300

    Add files via upload

---
+++ Aula0307/test/TestExterno.java
@@ -0,0 +1,28 @@
+package test;
+
+import controller.Extenso;
+
+public class TestExterno {
+
+	public static void main(String[] args) {
+		Extenso ext = new Extenso();
+		System.out.println(ext.porExt(2));
+		System.out.println(ext.porExt(8));
+		System.out.println(ext.porExt(11));
+		System.out.println(ext.porExt(5));
+		System.out.println(ext.porExt(-3));
+		System.out.println(ext.porExt(-33));
+		System.out.println(ext.porExt(-25));
+		System.out.println(ext.porExt(87));
+		System.out.println(ext.porExt(35));
+		System.out.println(ext.porExt(45));
+		System.out.println(ext.porExt(55));
+		System.out.println(ext.porExt(102));
+		System.out.println(ext.porExt(100));
+		System.out.println(ext.porExt(211));
+		System.out.println(ext.porExt(345));
+		System.out.println(ext.porExt(567));
+		System.out.println(ext.porExt(999));
+	}
+
+}
