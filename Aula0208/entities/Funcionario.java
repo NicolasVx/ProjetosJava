6dab6a425a5c42675439c7b77fbbbe2922f5211d
Nicolas <146778076+NicolasVx@users.noreply.github.com>
2024-08-02 11:46:12 -0300

    Add files via upload

---
+++ Aula0208/entities/Funcionario.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Funcionario extends Pessoa{  //extends: herança da classe pessoa, pegou todos os atributos
+	private String cargo;
+	private double salario;
+	
+	public Funcionario() {
+		super();
+		cargo = null;
+		salario = 0.0;
+	}
+	
+
+	public Funcionario(int id, String nome, String email, String sexo, String cargo, double salario) {
+		super(id, nome, email, sexo);
+		this.cargo = cargo;
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Funcionario [cargo=" + cargo + ", salario=" + salario + "]";
+	}
+	
+}
