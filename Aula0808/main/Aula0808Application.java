a36efa5a660afc137a09fd06e1a98fa2bab29c42
Nicolas <146778076+NicolasVx@users.noreply.github.com>
2024-08-08 11:40:09 -0300

    Add files via upload

---
+++ Aula0808/main/Aula0808Application.java
@@ -0,0 +1,13 @@
+package com.example.aula0808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula0808Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula0808Application.class, args);
+	}
+
+}
---
+++ Aula0808/main/Cliente.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id; // long: número inteiro grande
+	@Column(nullable = false)
+	private String Nome;
+	private String Email;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+}
